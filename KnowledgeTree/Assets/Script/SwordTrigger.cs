dd3454a15d9af08c3dc7e1764ff61eddc115bd8d
Helga Boufos <leneri.fos@gmail.com>
2021-07-07 08:13:25 +0300

    Add AnimationText

---
+++ KnowledgeTree/Assets/Script/SwordTrigger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+
+public class SwordTrigger : MonoBehaviour
+{
+     [SerializeField]private Animator textAnimation;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        textAnimation.SetTrigger("Trig");
+    }
+}
