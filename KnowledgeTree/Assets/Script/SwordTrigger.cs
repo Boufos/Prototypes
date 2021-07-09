34db1ebff0d83dc4ac012bef3f698f36711b758a
Helga Boufos <leneri.fos@gmail.com>
2021-07-09 07:44:58 +0300

    AddFont

--- KnowledgeTree/Assets/Script/SwordTrigger.cs
+++ KnowledgeTree/Assets/Script/SwordTrigger.cs
@@ -6,9 +6,26 @@ using UnityEngine.Animations;
 public class SwordTrigger : MonoBehaviour
 {
      [SerializeField]private Animator textAnimation;
+      public GameObject sword;
+
 
     private void OnTriggerEnter(Collider other)
     {
         textAnimation.SetTrigger("Trig");
+        
     }
+    private void OnTriggerStay(Collider other)
+    {
+        if (Input.GetKey(KeyCode.F))
+        {
+            Destroy(sword);
+            textAnimation.SetTrigger("Trig");
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        textAnimation.SetTrigger("Trig");
+    }
+
+   
 }
