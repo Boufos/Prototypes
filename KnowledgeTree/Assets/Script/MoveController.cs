cd502d5a66f88b644760816dc41d230da6d5461b
Helga Boufos <leneri.fos@gmail.com>
2021-07-14 00:31:05 +0300

    Finish

--- KnowledgeTree/Assets/Script/MoveController.cs
+++ KnowledgeTree/Assets/Script/MoveController.cs
@@ -9,11 +9,15 @@ public class MoveController : MonoBehaviour
     private Vector3 moveVector;
     private CharacterController ch_controller;
     private Animator ch_animator;
-
+    private AudioSource m_AudioSource;
+    public AudioClip[] step;
+    private float nextActionTime = 0f;
+    private float period = 0.3f;
     void Start()
     {
         ch_controller = GetComponent<CharacterController>();
         ch_animator = GetComponent<Animator>();
+        m_AudioSource = GetComponent<AudioSource>();
     }
 
     void Update()
@@ -27,7 +31,14 @@ public class MoveController : MonoBehaviour
         moveVector.z = Input.GetAxis("Vertical") * speedMove;
 
         if (moveVector.x != 0 || moveVector.z != 0)
+{
             ch_animator.SetBool("Move", true);
+            if (Time.time > nextActionTime)
+            {
+                nextActionTime += period;
+                PlayFootStepAudio();
+            }
+        }
         else ch_animator.SetBool("Move", false);
 
         transform.Rotate(0, moveVector.x * rotationMove, 0);
@@ -36,4 +47,13 @@ public class MoveController : MonoBehaviour
         float curSpedd = speedMove * moveVector.z;
         ch_controller.SimpleMove(forward * curSpedd);
     }
+    private void PlayFootStepAudio()
+    {
+        int n = Random.Range(1, step.Length);
+        m_AudioSource.clip = step[n];
+        m_AudioSource.PlayOneShot(m_AudioSource.clip);
+        step[n] = step[0];
+        step[0] = m_AudioSource.clip;
+    }
+
 }
